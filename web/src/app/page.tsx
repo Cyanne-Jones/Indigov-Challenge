f7c97129a61d67e975ffea9ac8da2ad10f6f4732
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 15:29:25 -0700

    pull constituent data on the front end

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,21 +2,34 @@
 
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import { Representative } from "../typeDefs/typeDef";
+import { Representative, Constituent } from "../typeDefs/typeDef";
 
 
 export default function Home() {
-  const [representative, setRepresentative] = useState<Representative | null>(null);
+  const [ representative, setRepresentative ] = useState<Representative | null>(null);
+  const [ constituents, setConstituents ] = useState<Constituent[] | null>(null);
 
-  // would pull this from auth login info in a real app
+  // would pull this from auth login/app state info in a real app
   const representativeId = 2;
 
   useEffect(() => {
-    // fetch the representative from the API
+    // fetch the representative from the backend
     fetch(`http://localhost:4000/representative?id=${representativeId}`)
       .then(res => res.json())
       .then((data) => {
         setRepresentative(data);
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+  }, []);
+
+  useEffect(() => {
+    // fetch the constituents from the backend
+    fetch(`http://localhost:4000/constituents?id=${representativeId}`)
+      .then(res => res.json())
+      .then((data) => {
+        setConstituents(data);
       })
       .catch((e) => {
         console.error(e);
