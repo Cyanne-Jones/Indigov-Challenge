48a32fa165eef6775d1f61949b4e4fb6dfb4e04e
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 16:56:29 -0700

    Parse json body, create add constituent endpoint

--- api/src/express.ts
+++ api/src/express.ts
@@ -1,23 +1,32 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import { getRepresentative } from "./services/representative";
-import { getAllConstituents } from "./services/constituents";
+import { getAllConstituents, createConstituent } from "./services/constituents";
 const app = express();
 const port = 4000;
 
 app.use(cors({
   origin:"*",
-  methods:['GET']
-}))  // Enable CORS for all routes
+  methods:['GET', 'POST']
+}));
+
+app.use(bodyParser.json());
 
 app.get('/representative', (req, res) => {
   const representative = getRepresentative(req?.query?.id);
-  return res.send(representative)
+  return res.send(representative);
 });
 
 app.get('/constituents', (req, res) => {
-  const constituents = getAllConstituents(req?.query?.id);
-  return res.send(constituents)
+  const constituents = getAllConstituents(req?.query?.representativeId);
+  return res.send(constituents);
+});
+
+app.post('/addConstituent', (req, res) => {
+  const constituent = req?.body?.constituent;
+  const constituents = createConstituent(constituent);
+  return res.send(constituents);
 });
 
 app.listen(port, () => {
