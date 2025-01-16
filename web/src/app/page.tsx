6bd1f612f2b51e8eb833fbc7c7ef10f712f2a420
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 12:58:15 -0700

    request representative name and display

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,95 +1,34 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import { Representative } from "./typeDefs/representative.typeDef";
+
 
 export default function Home() {
+  const [representative, setRepresentative] = useState<Representative | null>(null);
+
+  // would pull this from auth login info in a real app
+  const representativeId = 2;
+
+  useEffect(() => {
+    // fetch the representative from the API
+    fetch(`http://localhost:4000/representative?id=${representativeId}`)
+      .then(res => res.json())
+      .then((data) => {
+        setRepresentative(data);
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+  }, []);
+
+  const welcomeMessage = representative ? `Welcome back, ${representative.role} ${representative.name}!` : 'Welcome back!';
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <h1>{welcomeMessage}</h1>
+      <p>View and manage your constituents</p>
     </div>
   );
 }
