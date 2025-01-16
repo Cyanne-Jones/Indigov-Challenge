3efc410a9430e25683dc08cd2c2c0daf812ff086
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 13:42:09 -0700

    add an add constituent page

---
+++ web/src/app/add/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+const AddConstituentPage = () => {
+
+  return (
+    <div>
+      <h1>Add a constituent here!</h1>
+      <Button variant="contained" color="primary">Add Constituent</Button>
+    </div>
+  )
+}
+
+export default AddConstituentPage;
