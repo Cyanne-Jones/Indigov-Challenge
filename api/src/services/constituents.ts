e35d80c8514db08c7d59a265cf3a7d13e10412cc
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 15:29:06 -0700

    use filter instead of find, use parseInt on id passed in

--- api/src/services/constituents.ts
+++ api/src/services/constituents.ts
@@ -1,10 +1,10 @@
 import { constituents } from "../data/constituent";
 import { Constituent } from "../data/typeDefs";
 
-export const getAllConstituents = (representativeId: number) => {
+export const getAllConstituents = (representativeId: string) => {
   try {
     if (representativeId) {
-      const repConstituents = constituents.find((constituent: Constituent ) => constituent.representative_id === representativeId);
+      const repConstituents = constituents.filter((constituent: Constituent ) => constituent.representative_id === parseInt(representativeId));
       return repConstituents
     }
     throw new Error('No representative ID passed in');
