3ede8888f046fc1f582cf1681c62949eaf94eed5
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 10:47:54 -0700

    Bootstrap express.js

---
+++ api/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const port = 4000;
+
+app.use(cors({
+  origin:"*",
+  methods:['GET']
+}))  // Enable CORS for all routes
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/kittens', (req, res) => {
+  console.log('GET /kittens');
+  res.send({ kittens: ['Fluffy', 'Whiskers', 'Paws'] })
+})
+
+app.listen(port, () => {
+  console.log(`Running an Express.js server on port ${port}`)
+})
