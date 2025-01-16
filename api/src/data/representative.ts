a97971f98d321b28399135f743209701cfcc51af
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 11:58:20 -0700

    Add representative types and dummy data

---
+++ api/src/data/representative.ts
@@ -0,0 +1,66 @@
+export enum RepresentativeRole {
+  CONGRESSPERSON = "Congressperson",
+  SENATOR = "Senator",
+  GOVERNOR = "Governor",
+  MAYOR = "Mayor",
+  STATE_SENATOR = "State Senator",
+  STATE_CONGRESSPERSON = "State Congressperson",
+};
+
+export enum Party {
+  DEMOCRAT = "Democrat",
+  REPUBLICAN = "Republican",
+  INDEPENDENT = 'Independent',
+  GREEN_PARTY = 'Green Party',
+  LIBERTARIAN = 'Libertarian',
+  OTHER = 'Other',
+}
+
+export type Representative = {
+  name: string;
+  email: string;
+  phone: string;
+  id: number;
+  dateJoined: string;
+  role: RepresentativeRole;
+  party: Party;
+  // Add type safety for state and city if given more time
+  state: string;
+  city: string;
+};
+
+export const representatives: Representative[] = [
+  {
+    name: 'John Doe',
+    email: 'j.doe@congress.gov',
+    phone: '555-555-5555',
+    id: 1,
+    dateJoined: '2024-01-01',
+    role: RepresentativeRole.CONGRESSPERSON,
+    party: Party.DEMOCRAT,
+    state: 'CA',
+    city: 'Los Angeles'
+  },
+  {
+    name: 'Jane Doe',
+    email: 'j.doe@congress.gov',
+    phone: '555-555-5555',
+    id: 2,
+    dateJoined: '2024-01-01',
+    role: RepresentativeRole.CONGRESSPERSON,
+    party: Party.DEMOCRAT,
+    state: 'WA',
+    city: 'Seattle'
+  },
+  {
+    name: 'Tom Smith',
+    email: 'j.doe@congress.gov',
+    phone: '555-555-5555',
+    id: 1,
+    dateJoined: '2024-01-01',
+    role: RepresentativeRole.SENATOR,
+    party: Party.REPUBLICAN,
+    state: 'VA',
+    city: 'Richmond'
+  },
+]
