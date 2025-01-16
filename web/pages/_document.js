b058d2c546d578341fdbfff803c2e841141342c8
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-16 13:41:57 -0700

    update document

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -14,6 +14,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <body>
         <Main />
