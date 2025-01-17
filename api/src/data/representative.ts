46f5ca6d930c928b12b7ca066c6df75c8853c407
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-17 09:49:24 -0700

    update representatives

--- api/src/data/representative.ts
+++ api/src/data/representative.ts
@@ -2,19 +2,19 @@ import { Representative, RepresentativeRole, Party } from './typeDefs';
 
 export const representatives: Representative[] = [
   {
-    name: 'John Doe',
-    email: 'j.doe@congress.gov',
+    name: 'Benjamin Franklin',
+    email: 'b.franklin@congress.gov',
     phone: '555-555-5555',
     id: 1,
     date_joined: '2024-01-01',
     role: RepresentativeRole.CONGRESSPERSON,
-    party: Party.DEMOCRAT,
+    party: Party.INDEPENDENT,
     state: 'CA',
     city: 'Los Angeles'
   },
   {
-    name: 'Jane Doe',
-    email: 'j.doe@wa-congress.gov',
+    name: 'Jean Luc Picard',
+    email: 'j.picard@wa-congress.gov',
     phone: '666-666-6666',
     id: 2,
     date_joined: '2024-01-01',
@@ -24,8 +24,8 @@ export const representatives: Representative[] = [
     city: 'Seattle'
   },
   {
-    name: 'Tom Smith',
-    email: 't.smith@senate.gov',
+    name: 'Paul Revere',
+    email: 'paul.revere@wa-congress.gov',
     phone: '777-777-7777',
     id: 3,
     date_joined: '2024-01-01',
